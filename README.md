e548d97c9ac6c33e39addfff0b7aebf3e989cd9a
Lubna <shaikhls2421@gmail.com>
2022-07-24 12:57:25 +0530

    README.md file created

---
+++ README.md
@@ -0,0 +1,29 @@
+
+Exercise ✅ to get Response from the APIs.
+
+🟢 Following Steps 👇
+
+➡  fetch('https://www.superheroapi.com/api.php/749814503011060/') 👈👈 Standerd url(API)
+
+
+➡ 👉 Superhero API's Base Url 
+const TOKEN = '749814503011060'  // 👈 token for the API
+
+
+➡ 👉 Getting HTML elements by their IDs 
+➡ 👉👉  Random superhero Functionallity. 👈👈
+➡ 👉 Button to get Random Superhero  
+
+➡ 👉 Random Fuctionallity to get random superhero among 731 superheros 
+➡ 👉 adding onclick event to 'newSuperHeroBtn' and getSuperHero() function to it 
+
+➡ 👉 Searching super hero with name >>Functionallity.  
+
+➡ 👉 getting input as 'searchInput' and button as 'search' 
+
+
+➡ 👉 Search function which takes name as an argument 
+
+
+➡ 👉 onclick event listener to the search button and passing searchSuperHero() to it 
+
--- script.js
+++ script.js
@@ -1,14 +1,13 @@
+//  fetch('https://www.superheroapi.com/api.php/749814503011060/') 👈👈 Standerd url(API)
 
 
-
+// 👉 Superhero API's Base Url 👇👇
 const BASE_URL = 'https://www.superheroapi.com/api.php'
 const TOKEN = '749814503011060'  // 👈 token for the API
 
 
 
-//  fetch('https://www.superheroapi.com/api.php/749814503011060/') 👈👈 Standerd url(API)
-// 👉👉  Random superhero Functionallity.
-
+// 👉 Getting HTML elements by their IDs 👇👇
 const imgDiv = document.getElementById('imgDiv')
 const superheroName = document.getElementById('name')
 const durability = document.getElementById('durability')
@@ -16,60 +15,66 @@ const intelligence = document.getElementById('intelligence')
 const speed = document.getElementById('speed')
 const strength = document.getElementById('strength')
 
-const getSuperHero = (id)=>{
+// 👉👉  Random superhero Functionallity. 👈👈
+const getSuperHero = (id) => {
 
- fetch(`${BASE_URL}/${TOKEN}/${id}`)  // 👈👈 Requesting Responce to the API
- .then(response => response.json())   //👈👈 JSON Responce
- .then(json=> {
-    console.log(json)
-    imgDiv.innerHTML = `<img src= "${json.image.url}"  >`;
-    superheroName.innerHTML = `<h1> ${json.name} </h1>`
-    durability.innerHTML = `<p> Durability🤜 : ${json.powerstats.durability}</p>`
-    intelligence.innerHTML = `<p> Intelligence🧠 : ${json.powerstats.intelligence}</p>`
-    speed.innerHTML = `<p> Speed⏲ : ${json.powerstats.speed}</p>`
-    strength.innerHTML = `<p> Strength💪 : ${json.powerstats.strength}</p>`
+    fetch(`${BASE_URL}/${TOKEN}/${id}`)  // 👈👈 Requesting Responce to the API
+        .then(response => response.json())   //👈👈 JSON Responce
+        .then(json => {
+            console.log(json)
+            imgDiv.innerHTML = `<img src= "${json.image.url}"  >`;
+            superheroName.innerHTML = `<h1> ${json.name} </h1>`
+            durability.innerHTML = `<p> Durability🤜 : ${json.powerstats.durability}</p>`
+            intelligence.innerHTML = `<p> Intelligence🧠 : ${json.powerstats.intelligence}</p>`
+            speed.innerHTML = `<p> Speed⏲ : ${json.powerstats.speed}</p>`
+            strength.innerHTML = `<p> Strength💪 : ${json.powerstats.strength}</p>`
+        }
+
+        )
 }
 
-    )
-}
 
-// getSuperHero(309) //checking
-
+// 👉 Button to get Random Superhero  👇👇
 const newSuperHeroBtn = document.getElementById('superHerobtn')
-const randomHero  = ()=>{
+
+// 👉 Random Fuctionallity to get random superhero among 731 superheros 👇👇
+const randomHero = () => {
     let herosCount = 731
-   return Math.floor( Math.random()*herosCount) +1
-} 
-newSuperHeroBtn.onclick =  ()=> getSuperHero(randomHero())
+    return Math.floor(Math.random() * herosCount) + 1
+}
+
+
+// 👉 adding onclick event to 'newSuperHeroBtn' and getSuperHero() function to it 👇👇
+newSuperHeroBtn.onclick = () => getSuperHero(randomHero())
 
 
 
-// search Super Hero Functionality
 
+// 👉 Searching super hero with name >>Functionallity.  👇👇
+
+// 👉 getting input as 'searchInput' and button as 'search' 👇👇
 const searchInput = document.getElementById('searchInput')
 const search = document.getElementById('search')
 
-const  searchSuperHero= (name)=>{
-console.log(search.value)
-
+// 👉 Search function which takes name as an argument 👇👇
+const searchSuperHero = (name) => {
 
     fetch(`${BASE_URL}/${TOKEN}/search/${name}`)
-    .then(response => response.json())
-    .then(json =>{
-        // console.log(json)
-        const hero = json.results[0]
-        console.log(hero)
-        imgDiv.innerHTML = `<img src= "${hero.image.url}" >`;
-        superheroName.innerHTML = `<h1> ${hero.name} </h1>`
-        durability.innerHTML = `<p> Durability🤜 : ${hero.powerstats.durability}</p>`
-    intelligence.innerHTML = `<p> Intelligence🧠 : ${hero.powerstats.intelligence}</p>`
-    speed.innerHTML = `<p> Speed⏲ : ${hero.powerstats.speed}</p>`
-    strength.innerHTML = `<p> Strength💪 : ${hero.powerstats.strength}</p>`
-    
-    })
+        .then(response => response.json())
+        .then(json => {
+            const hero = json.results[0]
+            console.log(hero)
+            imgDiv.innerHTML = `<img src= "${hero.image.url}" >`;
+            superheroName.innerHTML = `<h1> ${hero.name} </h1>`
+            durability.innerHTML = `<p> Durability🤜 : ${hero.powerstats.durability}</p>`
+            intelligence.innerHTML = `<p> Intelligence🧠 : ${hero.powerstats.intelligence}</p>`
+            speed.innerHTML = `<p> Speed⏲ : ${hero.powerstats.speed}</p>`
+            strength.innerHTML = `<p> Strength💪 : ${hero.powerstats.strength}</p>`
 
+        })
 
 }
 
 
-search.onclick = ()=>  searchSuperHero(searchInput.value)
+// 👉 onclick event listener to the search button and passing searchSuperHero() to it 👇👇
+search.onclick = () => searchSuperHero(searchInput.value)
